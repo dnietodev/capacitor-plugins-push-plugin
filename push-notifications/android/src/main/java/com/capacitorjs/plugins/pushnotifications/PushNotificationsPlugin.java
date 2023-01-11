6951d9891f94a81c7b0757de72126d2635c8ab13
dnietodev <71189020+dnietodev@users.noreply.github.com>
2023-01-11 09:34:32 +0000

    ADD pending intent on push received when app in foreground

--- push-notifications/android/src/main/java/com/capacitorjs/plugins/pushnotifications/PushNotificationsPlugin.java
+++ push-notifications/android/src/main/java/com/capacitorjs/plugins/pushnotifications/PushNotificationsPlugin.java
@@ -2,6 +2,7 @@ package com.capacitorjs.plugins.pushnotifications;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -63,11 +64,28 @@ public class PushNotificationsPlugin extends Plugin {
                     dataObject.put(key, valueStr);
                 }
             }
-            notificationJson.put("data", dataObject);
+            notificationJson.put("data", data);
             JSObject actionJson = new JSObject();
             actionJson.put("actionId", "tap");
             actionJson.put("notification", notificationJson);
             notifyListeners("pushNotificationActionPerformed", actionJson, true);
+        } else if (bundle != null) {
+          JSObject notificationJson = new JSObject();
+          JSObject dataObject = new JSObject();
+          for (String key : bundle.keySet()) {
+            Object value = bundle.get(key);
+            if (key.equals("data")) {
+              dataObject.put(key, value);
+            } else {
+              String valueStr = (value != null) ? value.toString() : null;
+              dataObject.put(key, valueStr);
+            }
+          }
+          notificationJson.put("data", dataObject);
+          JSObject actionJson = new JSObject();
+          actionJson.put("actionId", "tap");
+          actionJson.put("notification", notificationJson);
+          notifyListeners("pushNotificationActionPerformed", actionJson, true);
         }
     }
 
@@ -212,7 +230,14 @@ public class PushNotificationsPlugin extends Plugin {
             data.put(key, value);
         }
         remoteMessageData.put("data", data);
-
+        Intent intent = new Intent(getContext(), getActivity().getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("data", data.toString());
+        int flags = PendingIntent.FLAG_CANCEL_CURRENT;
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+          flags = flags | PendingIntent.FLAG_MUTABLE;
+        }
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), remoteMessage.getMessageId().hashCode(), intent, flags);
         RemoteMessage.Notification notification = remoteMessage.getNotification();
         if (notification != null) {
             String title = notification.getTitle();
@@ -241,6 +266,8 @@ public class PushNotificationsPlugin extends Plugin {
                         .setSmallIcon(pushIcon)
                         .setContentTitle(title)
                         .setContentText(body)
+                        .setAutoCancel(true)
+                        .setContentIntent(pendingIntent)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT);
                     notificationManager.notify(0, builder.build());
                 }
